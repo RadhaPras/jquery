ed20c844a4e14783e491795a572fdbc06103fc6e
RadhaPras <56018650+RadhaPras@users.noreply.github.com>
2019-10-06 14:52:48 +0530

    Add files via upload

---
+++ Assig1.java
@@ -0,0 +1,22 @@
+public class Assig1 {
+	static
+	{
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver","./driver/geckodriver.exe");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		 WebDriver driver = new ChromeDriver();
+		 
+		driver.manage().window().maximize();
+		driver.get("http://www.jqueryui.com");
+		driver.findElement(By.linkText("Demos")).click();
+		driver.findElement(By.xpath("(//a[text()='Draggable'])")).click();
+		driver.findElement(By.linkText("Droppable")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("a[href='https://jqueryui.com/resizable/']")).click();
+		driver.findElement(By.xpath("(//a[text()='Sortable'])")).click();
+		
+	}
+
+}
